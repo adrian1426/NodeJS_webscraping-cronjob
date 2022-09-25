228c321923751091b3baecb03a336b2c7b4eeb4c
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-09-25 18:58:15 -0500

    Agregando funcion para leer sitio cnn y procesarlo, obtener los titulos y guardarlo en object

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const mongoose = require('mongoose');
+const axios = require('axios').default;
+const cheerio = require('cheerio');
 const { MONGO_URI } = require('./config');
-
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true });
 
-const CatModel = mongoose.model("Cat", { name: String });
+const loadWebSiteCnn = async () => {
+  const htmlCnn = await axios.get('https://cnnespanol.cnn.com/');
+  const loadHtml = cheerio.load(htmlCnn.data);
+  const titlesCnn = loadHtml('.news__title');
 
-const kitty = new CatModel({ name: 'Kawakito2' });
-kitty.save()
-  .then(result => console.log("save: ", result))
-  .catch(err => console.log(err));
+  titlesCnn.each((index, element) => {
+    const breakingNew = {
+      title: loadHtml(element).text().trim(),
+      link: loadHtml(element).children().attr('href')
+    };
+
+    console.log(breakingNew)
+  });
+};
+
+loadWebSiteCnn();
